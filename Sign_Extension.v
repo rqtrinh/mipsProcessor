7e0451b5aecc24e6c4b14ff94e435c209350ab0f
Ryan Trinh <rqtrinh@cpp.edu>
2022-12-04 15:04:18 -0800

    Added comments to Sign_Extension.v

--- Sign_Extension.v
+++ Sign_Extension.v
@@ -1,11 +1,17 @@
+// This module will sign extend to 32 bits
 module Sign_Extension(
 	bits16_in,
 	bits32_out
     );
-// Input from either sw or lw (16 bits)
+// The offsett of lw or sw (16 bits)
 input [15:0] bits16_in;
+// Output the sign extended 16 bit input (32 bits)
 output wire [31:0] bits32_out;
 
+// Get the highest bit of bits16_in
+// Fill the upper 16 bits of bits32_out with the highest bit of bits16_in
+// Make 16bits_in the lower 16 bits
+// Total 32 bits
 assign bits32_out = {{16{bits16_in[15]}} , bits16_in[15:0]};
 
 endmodule
