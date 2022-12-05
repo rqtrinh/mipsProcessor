9c2be34acce5b86377b683671469e71b02863bd4
Ryan Trinh <rqtrinh@cpp.edu>
2022-12-05 13:20:44 -0800

    Implemented jump capability

--- Processor_top.v
+++ Processor_top.v
@@ -1,9 +1,11 @@
 `include "Program_Counter.v"
 `include "Adder.v"
+`include "Concat_Jump_Addr"
 `include "Instruction_Memory.v"
 `include "Register_File.v"
 `include "Sign_Extension.v"
-`include "Shifter.v"
+`include "Shifter_Branch.v"
+`include "Shifter_Jump.v"
 `include "Alu_Top.v"
 `include "Data_Memory.v"
 `include "Control_Logic.v"
@@ -24,6 +26,8 @@ wire [31:0] addr_incr;
 wire [31:0] address_plus_4;
 wire [31:0] branch_addr_offset;
 wire [31:0] branch_address;
+wire [31:0] jump_address;
+wire [27:0] j_lower_28;
 wire [31:0] instrn;
 wire ctrl_write_en;
 wire final_write_en;
@@ -63,6 +67,12 @@ Adder adder_branch_addr (
 .out (branch_address)
 );
 
+Concat_Jump_Addr concat_jump_addr (
+.address_plus_4(address_plus_4)
+.bits28_in(j_lower_28)
+.jump_address(jump_address)
+)
+
 Instruction_Memory instr_mem (
 .instrn_address (out_address),
 .instrn (instrn[31:0])
@@ -85,11 +95,18 @@ Sign_Extension sign_ext (
 .bits32_out (sign_ext_out)
 );
 
-Shifter shifter (
+Shifter_Branch shifter_branch (
 .indata (sign_ext_out),
 .shift_amt (2'd2),
 .shift_left (1'b1),
 .outdata (branch_addr_offset)
+);
+
+Shifter_Jump shifter_jump (
+.indata (instrn[25:0]),
+.shift_amt (2'd2),
+.shift_left (1'b1),
+.outdata (j_lower_28)
 );
 
 Alu_Top alu (
@@ -114,6 +131,7 @@ Control_Logic ctrl_logic (
 .instrn_opcode   (instrn[31:26]),
 .address_plus_4  (address_plus_4),
 .branch_address  (branch_address),
+.jump_address    (jump_address),
 .ctrl_in_address (ctrl_in_address),
 .alu_result      (alu_result),
 .zero_out        (zero_out),
